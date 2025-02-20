fcdb9433bcb2c83bd908d4741ccd40016293796c
Karim <bungarik@gmail.com>
2025-02-19 18:04:38 -0600

    Initial commit

---
+++ src/main/java/com/tasker/email_sender/services/EmailTaskConsumer.java
@@ -0,0 +1,23 @@
+package com.tasker.email_sender.services;
+
+
+import com.common.EmailSendingDto;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmailTaskConsumer {
+
+    private EmailService emailService;
+
+    @Autowired
+    public EmailTaskConsumer(EmailService emailService) {
+        this.emailService = emailService;
+    }
+
+    @KafkaListener(topics = "EMAIL_SENDING_TASKS", groupId = "email-sender-group")
+    public void processEmailTask(EmailSendingDto task) {
+        emailService.sendEmail(task);
+    }
+}
